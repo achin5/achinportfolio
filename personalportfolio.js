64f87b3ba409f73040786409914b439d9877c9da
Aaron Chin <aaron.chin5@gmail.com>
2021-03-14 18:08:30 -0600

    Added movie posters in JS. Started working on CSS

--- personalportfolio.js
+++ personalportfolio.js
@@ -8,14 +8,19 @@ import { films } from '../data/films.js'
 
 // console.log(films[0].title);
 
-let titlelist = document.querySelector('.titleList')
+let movieList = document.querySelector('.movieList')
 
 for (let i = 0; i < films.length; i++) {
     const foundFilm = films.find(film => getLastNumber(film.url) === (i +1))
+    let figure = document.createElement('figure')
+    let newImage = document.createElement('img')
+    let figCaption = document.createElement('figcaption')
+    newImage.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
+    figCaption.textContent = foundFilm.title
 
-    let newItem = document.createElement('li')
-    newItem.textContent = foundFilm.title
-    titlelist.appendChild(newItem)
+    figure.appendChild(newImage)
+    figure.appendChild(figCaption)
+    movieList.appendChild(figure)
 }
 
 function getLastNumber(url) {
