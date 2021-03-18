404747294f53c3cd46f73475ef54c930bb02743f
Aaron Chin <aaron.chin5@gmail.com>
2021-03-17 19:19:45 -0600

    Added images into JS. Started CSS on characters.

--- characters/main.js
+++ characters/main.js
@@ -5,6 +5,7 @@ const mainContent = document.querySelector('#main')
 people.forEach(person => {
     const charFigure = document.createElement('figure')
     const charImg = document.createElement('img')
+    charImg.src = `https://starwars-visualguide.com/assets/img/characters/1.jpg`
     const charCaption = document.createElement('figcaption')
 
     charCaption.textContent = person.name
