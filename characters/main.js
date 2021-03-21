76af99c2d5e1541f6368197d68dcb81edc05c50b
Aaron Chin <aaron.chin5@gmail.com>
2021-03-21 17:23:34 -0600

    created JS file to use for exporting loop

--- characters/main.js
+++ characters/main.js
@@ -1,4 +1,5 @@
 import { people } from '../data/people.js'
+import { getLastNumber, removeChildren } from '../utils/index.js'
 
 const mainContent = document.querySelector('#main')
 
@@ -71,21 +72,4 @@ function populateDOM(characters) {
     mainContent.appendChild(charFigure)
 
 })
-}
-
-function getLastNumber(url) {
-    let end = url.lastIndexOf('/')
-    let start = end - 2
-    if (url.charAt(start) === '/') {
-        start++
-    }
-    return url.slice(start, end)
-}
-
-// REMOVE CHILDREN
-function removeChildren(container) {
-    while (container.firstChild) {
-        container.removeChild(container.firstChild)
-    }
-
 }
--- starships/main.js
+++ starships/main.js
@@ -1,16 +1,30 @@
 import { starships } from '../data/starships.js'
+import { getLastNumber, removeChildren } from '../utils/index.js'
 
 const main = document.querySelector('main')
 const navList = document.querySelector('.navList')
-const shipView = documnet.querySelector('.shipView')
+const shipView = document.querySelector('.shipView')
 
 function populateNav() {
     starships.forEach((starship) => {
+        let anchorWrap = document.createElement('a')
+        anchorWrap.href = '#'
+        anchorWrap.addEventListener('click', () => populateShipView(starship))
         let listItem = document.createElement('li')
         listItem.textContent = starship.name
 
-        navList.appendChild(listItem)
+        anchorWrap.appendChild(listItem)
+        navList.appendChild(anchorWrap)
     })
 }
 
+function populateShipView(shipData) {
+    removeChildren(shipView)
+    let shipImage = document.createElement('img')
+    let shipNum = getLastNumber(shipData.url)
+    shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
+    shipView.appendChild(shipImage)
+}
+
 populateNav()
+populateShipView()
