a671f276a82065e5966f2a0e40b937aaeca07b53
Aaron Chin <aaron.chin5@gmail.com>
2021-03-21 18:26:38 -0600

    Added star field

--- starships/main.js
+++ starships/main.js
@@ -23,8 +23,36 @@ function populateShipView(shipData) {
     let shipImage = document.createElement('img')
     let shipNum = getLastNumber(shipData.url)
     shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
+    shipImage.addEventListener('error', () =>  {
+        shipImage.hidden = true
+    })
     shipView.appendChild(shipImage)
 }
 
 populateNav()
-populateShipView()
+
+function addStarField(element, numStars) {
+    element.style.setProperty('background-color', 'black')
+    for (let i = 0; i < numStars; i++) {
+        let star = document.createElement('div')
+        star.style.setProperty('width','2px')
+        star.style.setProperty('height','2px')
+        star.style.setProperty('background-color', 'white',)
+        let xy = getRandomPosition()
+        star.style.left = `${xy[0]}px`
+        star.style.top = `${xy[1]}px`
+        star.style.setProperty('position', 'absolute')
+    element.appendChild(star)
+  }
+}
+
+function getRandomPosition() {
+    let y = document.body.scrollHeight
+    let x = document.body.scrollWidth
+    let randomY = Math.floor(Math.random() * y)
+    let randomX = Math.floor(Math.random() * x)
+    return [randomX, randomY]
+}
+
+
+addStarField(document.querySelector('body'), 400)
