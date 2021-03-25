c9816d1c27b55ba9d38d634bb6a4884d57320455
Aaron Chin <aaron.chin5@gmail.com>
2021-03-25 16:37:23 -0600

    Finished CSS on films. Added wiki link on click.

--- films/main.js
+++ films/main.js
@@ -6,15 +6,20 @@ var x = document.getElementById("myHeader");
 let movieList = document.querySelector('.movieList')
 
 for (let i = 0; i < films.length; i++) {
-    const foundFilm = films.find(film => getLastNumber(film.url) === (i +1))
+    const foundFilm = films.find(film => getLastNumber(film.url) === (i + 1))
     let figure = document.createElement('figure')
     let newImage = document.createElement('img')
     let figCaption = document.createElement('figcaption')
     newImage.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
     figCaption.textContent = foundFilm.title
 
+
+
     figure.appendChild(newImage)
     figure.appendChild(figCaption)
     movieList.appendChild(figure)
 }
 
+movieList.addEventListener('click', () => {
+    window.open('https://en.wikipedia.org/wiki/Star_Wars');
+})
