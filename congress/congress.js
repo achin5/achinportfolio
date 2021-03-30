1ab86dddfe6274ad03c56edf17abc04f19884122
Aaron Chin <aaron.chin5@gmail.com>
2021-03-30 12:45:41 -0600

    Added congress photos.

--- congress/congress.js
+++ congress/congress.js
@@ -1,22 +1,58 @@
 import { senators } from "../data/senators.js"
 import { representatives } from "../data/representatives.js"
 
-const repubButton = document.querySelector('#republicans')
+const congressGrid = document.querySelector('.congressGrid')
+const seniority = document.querySelector('#seniority')
+const birthday = document.querySelector('#birthday')
 
-repubButton.addEventListener('click' , () => {
-    showRepub()
-})
+function populateCongressDiv(somearray) {
+    somearray.forEach(person => {
+        let personDiv = document.createElement('div')
+        personDiv.className = 'figureDiv'
+        let personFig = document.createElement('figure')
+        let figImg = document.createElement('img')
+        let figCaption = document.createElement('figcaption')
 
-function showRepub() {
-    // const repubs = representatives.filter(rep => rep.party === 'R')
-    const repubs = representatives.map (rep => {
-        let smallRepub = {}
-        if(rep.party === 'R'){
-          
-                smallRepub.id = rep.id
-                smallRepub.name = `${rep.first_name} ${rep.middle_name} ${rep.last_name}`
-            }
-            return smallRepub
-        })
-    console.log(repubs)
+        figImg.src = `https://www.govtrack.us/static/legislator-photos/412495-100px.jpeg`
+
+        figCaption.textContent = person.name
+
+        personFig.appendChild(figImg)
+        personFig.appendChild(figCaption)
+        personDiv.appendChild(personFig)
+        congressGrid.appendChild(personDiv)
+    })
 }
+
+function getSimplifiedPeople(peopleList) {
+    return peopleList.map(person => {
+        let middleName = person.middle_name ? `${person.middle_name}` : ``
+        return {
+            id: person.id,
+            name: `${person.first_name} ${person.middle_name} ${person.last_name}`,
+            imgURL: `https://www.govtrack.us/static/legislator-photos/${person.govtrack_id}-100px.jpeg`
+        }
+    })
+}
+
+populateCongressDiv(getSimplifiedPeople(representatives))
+
+// const repubButton = document.querySelector('#republicans')
+
+// repubButton.addEventListener('click' , () => {
+//     showRepub()
+// })
+
+// function showRepub() {
+//     // const repubs = representatives.filter(rep => rep.party === 'R')
+//     const repubs = representatives.map (rep => {
+//         let smallRepub = {}
+//         if(rep.party === 'R'){
+          
+//                 smallRepub.id = rep.id
+//                 smallRepub.name = `${rep.first_name} ${rep.middle_name} ${rep.last_name}`
+//             }
+//             return smallRepub
+//         })
+//     console.log(repubs)
+// }
