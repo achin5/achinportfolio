ad82475b11129656a2743ef7d915f84dc4b6e83b
Aaron Chin <aaron.chin5@gmail.com>
2021-04-29 17:34:58 -0600

    Added hamburger menu on media query projects page

--- personalportfolio.js
+++ personalportfolio.js
@@ -1,17 +1,5 @@
-import { films } from '../data/films.js'
-import { getLastNumber } from '../utils/index.js'
+const hamburger = document.getElementById('hamburger');
+const navList = document.getElementById('nav-list');
 
-let movieList = document.querySelector('.movieList')
+hamburger.addEventListener('click', () => navList.classList.toggle('show'))
 
-for (let i = 0; i < films.length; i++) {
-    const foundFilm = films.find(film => getLastNumber(film.url) === (i +1))
-    let figure = document.createElement('figure')
-    let newImage = document.createElement('img')
-    let figCaption = document.createElement('figcaption')
-    newImage.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
-    figCaption.textContent = foundFilm.title
-
-    figure.appendChild(newImage)
-    figure.appendChild(figCaption)
-    movieList.appendChild(figure)
-}
